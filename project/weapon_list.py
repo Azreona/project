b976885852be096be646b8011b9911d8d88d1846
Azreona <bennybigbucks87@gmail.com>
2024-08-28 16:11:48 +0200

    More changes

--- project/weapon_list.py
+++ project/weapon_list.py
@@ -2,6 +2,8 @@ import random
 
 #create enemy list and import "current enemy into this one module"
 #create current enemy 
+#create current enemy with health
+
 
 #weaponseffect
 
@@ -13,11 +15,14 @@ def pixie_dust(current_enemy):
         pixie_dust_effect = ["Exploded", "Imploded", "Turned to Stone", "Face Melted"]
         selected_effect = random.choice(pixie_dust_effect)
         enemy_health = 0
-        print(f"The {current_enemy} {selected_effect}!")
-    
+        print(f"The {current_enemy} {selected_effect}! The {current_enemy} is defeated.")
+    else:
+        print("A faint fizzle can be heard, nothing happened.")
+        
 def magical_brass_knuckles(current_enemy):
     magical_brass_knuckles_damage = (random.randrange(-10,30))
-        if magical_brass_knuckles_damage == (1,30):
+        if magical_brass_knuckles_damage == (1,30): 
+            enemy_health 
             
 
 
